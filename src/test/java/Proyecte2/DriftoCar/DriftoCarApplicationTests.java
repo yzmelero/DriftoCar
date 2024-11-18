ee75eb321dc29cb16872ca02f026f369aa655887
Mario Olaya Perez <mariolayap@gmail.com>
2024-11-18 11:53:08 +0100

    Inicio de DriftoCar

---
+++ src/test/java/Proyecte2/DriftoCar/DriftoCarApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecte2.DriftoCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriftoCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
