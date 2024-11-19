d8e88386a3dc24cb89e2e789c5677cecab173acc
Anna <aserrano@alumnat.copernic.cat>
2024-11-19 09:59:18 +0100

    Canviats tots els packages de "Proyecte2" a "Projecte2".

---
+++ src/main/java/Projecte2/DriftoCar/DriftoCarApplication.java
@@ -0,0 +1,13 @@
+package Projecte2.DriftoCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriftoCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriftoCarApplication.class, args);
+	}
+
+}
