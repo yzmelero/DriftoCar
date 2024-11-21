e38722e824681ffb804379269b54fc80a9464143
Anna <aserrano@alumnat.copernic.cat>
2024-11-21 10:09:10 +0100

    Classe client amb atributs i annotacions @Data, @AllArgsConstructor, @NoArgsConstructor, @Entity i @Comulmn. Falta revisar les relacions.

--- src/main/java/Projecte2/DriftoCar/entity/MySQL/Client.java
+++ src/main/java/Projecte2/DriftoCar/entity/MySQL/Client.java
@@ -4,10 +4,59 @@
  */
 package Projecte2.DriftoCar.entity.MySQL;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author Anna
  */
-public class Client {
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client /*extends Usuari*/{
+    
+    @Id
+    private String dni;
+    
+    @Column
+    private String nom;
+    
+    @Column
+    private String cognoms;
+    
+    @Column(unique = true)
+    private String email;
+    
+    @Column(unique = true)
+    private String llicencia;
+     
+    @Column(name = "llic_caducitat")
+    private LocalDate llicCaducitat;
+    
+    @Column(name = "dni_caducitat")
+    private LocalDate dniCaducitat;
+    
+    @Column(name = "num_tarjeta_credit", unique = true)
+    private int numTarjetaCredit;
+    
+    @Column
+    private String adreca;
+    
+    @Column
+    private String contrasenya;
+    
+    
+    
+    
     
 }
