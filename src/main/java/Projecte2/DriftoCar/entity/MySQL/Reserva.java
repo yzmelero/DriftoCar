cac3f413409c541de8eb15a6997aa2213bef1997
Héctor Vico García <vico.garcia.hector@alumnat.copernic.cat>
2024-11-21 11:47:09 +0100

    He fet la part de Reserva i falta modificar el HistoricReserves

--- src/main/java/Projecte2/DriftoCar/entity/MySQL/Reserva.java
+++ src/main/java/Projecte2/DriftoCar/entity/MySQL/Reserva.java
@@ -4,10 +4,57 @@
  */
 package Projecte2.DriftoCar.entity.MySQL;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author Anna
  */
+@Entity
+@Table(name = "reserves")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reserva {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long idReserva;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "client", referencedColumnName = "dni", nullable = false) 
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) 
+    @JoinColumn(name = "vehicle", referencedColumnName = "matricula", nullable = false) 
+    private Vehicle vehicle;
+
+    @Column(name = "data_inici", nullable = false)
+    private Date dataInici;
+
+    @Column(name = "data_fi", nullable = false)
+    private Date dataFi;
+
+    @Column(name = "cost_total", nullable = false)
+    private double costTotal; 
+
+    @Column(name = "fianca", nullable = false)
+    private double fianca;
+    
+    @Column(name = "estat", nullable = false)
+    private boolean estat;
+  
 }
