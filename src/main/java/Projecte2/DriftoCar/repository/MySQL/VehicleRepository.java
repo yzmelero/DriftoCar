357867c10e68f3bd02523e24b279dd3154226f17
YaizaMelero <ymelero@alumnat.copernic.cat>
2024-11-25 08:42:19 +0100

    Implementada funcionalitat per donar d'alta vehicles a VehicleService. A VehicleRepository, afegits mètodes per cercar vehicles segons els seus atributs.

--- src/main/java/Projecte2/DriftoCar/repository/MySQL/VehicleRepository.java
+++ src/main/java/Projecte2/DriftoCar/repository/MySQL/VehicleRepository.java
@@ -4,12 +4,32 @@
  */
 package Projecte2.DriftoCar.repository.MySQL;
 
-
+import Projecte2.DriftoCar.entity.MySQL.TipusVehicle;
+import Projecte2.DriftoCar.entity.MySQL.Vehicle;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Anna
  */
-public interface VehicleRepository {
-    
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, String> {
+
+    Optional<Vehicle> findByMatricula(String matricula);
+
+    List<Vehicle> findByMarca(String marca);
+
+    List<Vehicle> findByModel(String model);
+
+    List<Vehicle> findByDisponibilitat(boolean disponibilitat);
+
+    List<Vehicle> findByLocalitzacioCodiPostal(String codiPostal);
+
+    List<Vehicle> findByTipusAndDisponibilitat(TipusVehicle tipus, boolean disponibilitat);
+
+    List<Vehicle> findByAnyBetween(int startYear, int endYear);
+
 }
