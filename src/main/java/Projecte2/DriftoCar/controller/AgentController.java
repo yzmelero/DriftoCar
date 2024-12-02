6ccea3593844f4ae343a970288828c15aaf97c81
Mario Olaya Perez <mariolayap@gmail.com>
2024-12-02 12:16:26 +0100

    Pantallas existentes, peta si se pone una localizacion inexistente

--- src/main/java/Projecte2/DriftoCar/controller/AgentController.java
+++ src/main/java/Projecte2/DriftoCar/controller/AgentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import Projecte2.DriftoCar.entity.MySQL.Agent;
@@ -23,6 +25,18 @@ public class AgentController {
         List<Agent> agents = agentService.llistarAgents();
         model.addAttribute("agents", agents);
         return "agent-llistar"; // Nombre del archivo HTML
+    }
+    
+    @GetMapping("/alta")
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("agent", new Agent());
+        return "agent-alta"; // Nombre del archivo HTML
+    }
+
+    @PostMapping("/guardar")
+    public String guardarAgente(@ModelAttribute Agent agent) {
+        agentService.altaAgent(agent);
+        return "redirect:/agent/listar";
     }
 }
 
