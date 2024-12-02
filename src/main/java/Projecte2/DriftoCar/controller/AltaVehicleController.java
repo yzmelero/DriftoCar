55ba550b4010c6ca44a7680143add8bdafa11abb
YaizaMelero <ymelero@alumnat.copernic.cat>
2024-12-02 12:49:00 +0100

    Afegit l'HTML per llistar vehicles i afegit llistar a VehicleService. Creat el controlador per llistar a la classe AltaVehicleController.

--- src/main/java/Projecte2/DriftoCar/controller/AltaVehicleController.java
+++ src/main/java/Projecte2/DriftoCar/controller/AltaVehicleController.java
@@ -4,10 +4,33 @@
  */
 package Projecte2.DriftoCar.controller;
 
+import Projecte2.DriftoCar.entity.MySQL.Vehicle;
+import Projecte2.DriftoCar.service.MySQL.VehicleService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *
  * @author Anna
  */
+@Controller
+@RequestMapping("/vehicles")
 public class AltaVehicleController {
-    
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    // Llistar 
+    @GetMapping("/llistar")
+    public String llistarVehicles(Model model) {
+        List<Vehicle> vehicles = vehicleService.llistarVehicles();
+        model.addAttribute("vehicles", vehicles);
+        return "vehicle-llistar";
+    }
+
+
 }
