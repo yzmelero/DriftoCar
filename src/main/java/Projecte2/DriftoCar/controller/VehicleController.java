36bcd1e286f42634bc72cb2a138f2cb67703c0c5
YaizaMelero <ymelero@alumnat.copernic.cat>
2024-12-10 12:24:42 +0100

    Afegit a VehicleController el mètode de mostrar detalls per visualitzar els detalls del vehicle a una pàgina apart. Creada tambè la pantalla d'HTML i afegit que quan se selecciona la línia del vehicle que vols, et porta directe, sense necesitat d'un botó.

--- src/main/java/Projecte2/DriftoCar/controller/VehicleController.java
+++ src/main/java/Projecte2/DriftoCar/controller/VehicleController.java
@@ -62,4 +62,17 @@ public class VehicleController {
         vehicleService.baixaVehicle(matricula);
         return "redirect:/vehicle/llistar";
     }
+
+    @GetMapping("/detall/{matricula}")
+    public String mostrarVehicle(@PathVariable String matricula, Model model) {
+        Vehicle vehicle = vehicleService.obtenirVehicleMatricula(matricula);
+        if (vehicle == null) {
+            model.addAttribute("error", "No s'ha trobat vehicle amb matrícula: " + matricula);
+            return "redirect:/vehicle/llistar";
+        }
+        model.addAttribute("vehicle", vehicle);
+        List<Localitzacio> localitzacions = localitzacioService.llistarLocalitzacions();
+        model.addAttribute("localitzacions", localitzacions);
+        return "vehicle-detalls";
+    }
 }
