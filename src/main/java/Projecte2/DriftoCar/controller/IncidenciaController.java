be1d531d2b1c17ec87e86e53e4a2db11191e1f29
Héctor Vico García <vico.garcia.hector@alumnat.copernic.cat>
2024-12-18 10:16:38 +0100

    reparacio d'un error al fer merge

--- src/main/java/Projecte2/DriftoCar/controller/IncidenciaController.java
+++ src/main/java/Projecte2/DriftoCar/controller/IncidenciaController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -32,6 +33,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  */
 @Controller
 @RequestMapping("/incidencia")
+@Scope("session")
 public class IncidenciaController {
 
     @Autowired
