47b91a976a69ca77fb7af28105328f0d246231a3
Mario Olaya Perez <mariolayap@gmail.com>
2024-12-26 12:38:55 +0100

    Alta de agente con fotos funciona

--- src/main/java/Projecte2/DriftoCar/controller/AgentController.java
+++ src/main/java/Projecte2/DriftoCar/controller/AgentController.java
@@ -3,6 +3,7 @@ package Projecte2.DriftoCar.controller;
 import java.util.List;
 
 import org.slf4j.LoggerFactory;
+import org.bson.types.Binary;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -15,9 +16,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import Projecte2.DriftoCar.entity.MongoDB.DocumentacioClient;
 import Projecte2.DriftoCar.entity.MySQL.Agent;
 import Projecte2.DriftoCar.entity.MySQL.Localitzacio;
+import Projecte2.DriftoCar.repository.MongoDB.DocumentacioClientRepository;
 import Projecte2.DriftoCar.service.MySQL.AgentService;
 import Projecte2.DriftoCar.service.MySQL.LocalitzacioService;
 import jakarta.validation.Valid;
@@ -33,7 +37,9 @@ public class AgentController {
     @Autowired
     private LocalitzacioService localitzacioService;
     @Autowired
-    private PasswordEncoder passwordEncoder; 
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private DocumentacioClientRepository documentacioClientRepository;
 
     /**
      * Filtra els agents pel seu DNI.
@@ -69,8 +75,20 @@ public class AgentController {
         return "agent-alta";
     }
 
+    //TODO si hay un error en el dni, el telefono se pierde
+    /**
+     * Guarda un nuevo agente en la base de datos.
+     * @param agent El agente a guardar.
+     * @param bindingResult El resultado de la validación.
+     * @param model El modelo para pasar los datos a la vista.
+     * @param imatgeDni La imagen del DNI.
+     * @param imatgeLlicencia La imagen de la licencia.
+     * @return La vista de la lista de agentes si todo es correcto, o el formulario de alta si hay errores.
+     */
     @PostMapping("/guardar")
-    public String guardarAgente(@Valid Agent agent, BindingResult bindingResult, Model model) {
+    public String guardarAgente(@Valid Agent agent, BindingResult bindingResult, Model model,
+            @RequestParam(value = "imatgeDni", required = false) MultipartFile imatgeDni,
+            @RequestParam(value = "imatgeLlicencia", required = false) MultipartFile imatgeLlicencia) {
 
         if (bindingResult.hasErrors()) {
             List<Localitzacio> localitzacions = localitzacioService.llistarLocalitzacions();
@@ -78,11 +96,22 @@ public class AgentController {
             model.addAttribute("agent", agent);
             return "agent-alta";
         }
-
+        String contrasenya = agent.getContrasenya();
         try {
+            DocumentacioClient documentacio = new DocumentacioClient();
+            documentacio.setDni(agent.getDni());
+
+            if (imatgeDni != null && !imatgeDni.isEmpty()) {
+                documentacio.setImatgeDni(new Binary[] { new Binary(imatgeDni.getBytes()) });
+            }
+            if (imatgeLlicencia != null && !imatgeLlicencia.isEmpty()) {
+                documentacio.setImatgeLlicencia(new Binary[] { new Binary(imatgeLlicencia.getBytes()) });
+            }
+            documentacioClientRepository.save(documentacio);
             agentService.altaAgent(agent);
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
             model.addAttribute("error", e.getMessage());
+            agent.setContrasenya(contrasenya);
             model.addAttribute("agent", agent); // Mantiene los demás datos en el formulario
 
             List<Localitzacio> localitzacions = localitzacioService.llistarLocalitzacions();
@@ -117,7 +146,7 @@ public class AgentController {
     public String guardarAgentModificat(@Valid Agent agent, Model model) {
         Agent existent = agentService.obtenirAgentPerDni(agent.getDni());
         agent.setRol(existent.getRol());
-        if (agent.getNacionalitat()== null ||agent.getNacionalitat().isEmpty()) {
+        if (agent.getNacionalitat() == null || agent.getNacionalitat().isEmpty()) {
             agent.setNacionalitat(existent.getNacionalitat());
         }
         if (agent.getContrasenya() == null || agent.getContrasenya().isEmpty()) {
