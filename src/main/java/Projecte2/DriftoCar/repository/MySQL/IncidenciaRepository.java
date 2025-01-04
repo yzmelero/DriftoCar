c4f311bbb156028b79048360231697562ac2241a
Anna <aserrano@alumnat.copernic.cat>
2025-01-04 12:58:30 +0100

    Javadocs per a IncidenciaRepository fets.

--- src/main/java/Projecte2/DriftoCar/repository/MySQL/IncidenciaRepository.java
+++ src/main/java/Projecte2/DriftoCar/repository/MySQL/IncidenciaRepository.java
@@ -10,10 +10,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- *
- * @author Anna
+ * Repositori per gestionar les operacions CRUD de la taula `Incidencia` a la
+ * base de dades relacional.
+ * Extén {@link JpaRepository} per proporcionar funcionalitats predefinides.
  */
 @Repository
 public interface IncidenciaRepository extends JpaRepository<Incidencia, Long> {
+    /**
+     * Cerca una llista d'incidències segons el seu estat.
+     *
+     * @param estat l'estat de l'incidència (true = actiu, false = inactiu).
+     * @return una llista d'incidències amb l'estat especificat.
+     */
     List<Incidencia> findByEstat(boolean estat);
 }
