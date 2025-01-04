16074725a814e14c782447e36974250a04d9e94d
Anna <aserrano@alumnat.copernic.cat>
2025-01-04 17:44:03 +0100

    Logs i Javadocs per a ReservaService fets.

--- src/main/java/Projecte2/DriftoCar/service/MySQL/ReservaService.java
+++ src/main/java/Projecte2/DriftoCar/service/MySQL/ReservaService.java
@@ -27,8 +27,9 @@ import Projecte2.DriftoCar.repository.MySQL.VehicleRepository;
 import java.time.temporal.ChronoUnit;
 
 /**
- *
- * @author Mario
+ * Classe de servei per a la gestió de reserves de vehicles.
+ * Proporciona mètodes per a crear, modificar, desactivar i cercar reserves,
+ * així com calcular el preu total de les mateixes.
  */
 @Service
 public class ReservaService {
@@ -48,40 +49,47 @@ public class ReservaService {
     private HistoricReservesRepository historicReservesRepository;
 
     /**
-     * Aquest metode guarda una nova reserva a a BBDD si el client i la
-     * matricula existeixen
+     * Aquest metode guarda una nova reserva a la base de dades si el client i la
+     * matrícula existeixen.
      *
-     * @param reserva
-     * @return
-     * @throws Exception
-     * @author Mario
+     * @param reserva Reserva a guardar
+     * @return Reserva guardada
+     * @throws Exception si el vehicle o client no existeixen
      */
     public Reserva altaReserva(Reserva reserva) {
-        log.info("S'ha entrat al metode d'altaReserva.");
+        log.info("Iniciant el mètode altaReserva del servei amb reserva: {}", reserva);
 
+        log.debug("Comprovant si el vehicle existeix amb matrícula: {}", reserva.getVehicle().getMatricula());
         // Verificar que el vehículo existe
         Optional<Vehicle> vehicle = vehicleRepository.findByMatricula(reserva.getVehicle().getMatricula());
 
+        log.debug("Comprovant si el client existeix amb DNI: {}", reserva.getClient().getDni());
         // Verificar que el cliente existe
         Optional<Client> client = clientRepository.findByDni(reserva.getClient().getDni());
 
         if (client.isEmpty()) {
+            log.error("El client amb DNI {} no existeix", reserva.getClient().getDni());
             throw new RuntimeException("El client no existeix");
         }
         if (vehicle.isEmpty()) {
+            log.error("El vehicle amb matrícula {} no existeix", reserva.getVehicle().getMatricula());
             throw new RuntimeException("El vehicle no existeix");
         }
 
         Vehicle vehicleNou = vehicle.get();
         if (!vehicleNou.isDisponibilitat()) {
+            log.warn("El vehicle amb matrícula {} no està disponible", vehicleNou.getMatricula());
             throw new RuntimeException("El vehicle no esta disponible");
         }
 
+        log.info("Assignant client i vehicle a la reserva.");
         reserva.setClient(client.get());
         reserva.setVehicle(vehicle.get());
 
+        log.info("Guardant la reserva a la base de dades.");
         Reserva savedReserva = reservaRepository.save(reserva);
 
+        log.info("Creant el registre històric de la reserva.");
         // Crear el histórico después de guardar la reserva
         HistoricReserves historic = new HistoricReserves();
         historic.setIdReserva(savedReserva.getIdReserva().toString()); // Ahora el ID está disponible
@@ -95,54 +103,105 @@ public class ReservaService {
         historic.setFianca(calculFianca(savedReserva));
         historic.setEstat(savedReserva.isEstat());
 
+        log.debug("Guardant el registre històric a la base de dades.");
         historicReservesRepository.save(historic);
 
+        log.info("Reserva guardada correctament amb ID: {}", savedReserva.getIdReserva());
         return savedReserva;
     }
 
+    /**
+     * Obté totes les reserves registrades a la base de dades.
+     *
+     * @return Llista de reserves
+     */
     public List<Reserva> llistarReservas() {
+        log.info("Obtenint totes les reserves des de la base de dades.");
         return reservaRepository.findAll();
     }
 
+    /**
+     * Cerca una reserva per ID.
+     *
+     * @param idReserva ID de la reserva a cercar
+     * @return Reserva trobada
+     */
     public Optional<Reserva> cercaPerId(Long idReserva) {
-
+        log.info("Buscant reserva amb ID: {}", idReserva);
         return reservaRepository.findById(idReserva);
     }
 
+    /**
+     * Cerca reserves segons els filtres proporcionats (email, ID de reserva,
+     * matrícula).
+     *
+     * @param email      Email del client
+     * @param id_reserva ID de la reserva
+     * @param matricula  Matricula del vehicle
+     * @return Llista de reserves trobades
+     */
     public List<Reserva> cercarReserva(String email, Long id_reserva, String matricula) {
         log.debug("cercarReserva() - email: " + email + ", id_reserva: " + id_reserva + ", matricula: " + matricula);
         return reservaRepository.cercarReserves(id_reserva, email, matricula);
 
     }
 
+    /**
+     * Modifica una reserva existent.
+     *
+     * @param reserva Reserva amb les dades modificades
+     */
     public void modificarReserva(Reserva reserva) {
         log.info("Guardant reserva amb ID: " + reserva.getIdReserva());
         reservaRepository.save(reserva);
         log.info("Reserva guardada correctament.");
     }
 
+    /**
+     * Obté les reserves d'un vehicle segons la seva matrícula.
+     *
+     * @param matricula Matrícula del vehicle
+     * @return Llista de reserves
+     */
     public List<Reserva> obtenirReservesPerMatricula(String matricula) {
+        log.info("Buscant reserves per matrícula: {}", matricula);
         return reservaRepository.findByVehicleMatriculaEstat(matricula);
     }
 
+    /**
+     * Desactiva una reserva marcant-la com a no activa.
+     *
+     * @param idReserva ID de la reserva a desactivar
+     * @return Reserva desactivada
+     */
     public Reserva desactivarReserva(Long idReserva) {
+        log.info("Intentant desactivar la reserva amb ID: {}", idReserva);
         Reserva reserva = reservaRepository.findById(idReserva)
                 .orElseThrow(() -> new RuntimeException("Reserva " + idReserva + " no trobada: "));
         if (reserva.isEstat()) {
+            log.info("La reserva està activa. Procedint a desactivar-la.");
             reserva.setEstat(false); // Marca la reserva como inactiva
             HistoricReserves historic = historicReservesRepository.findById(reserva.getIdReserva().toString())
                     .orElseThrow(() -> new RuntimeException(
                             "Reserva " + idReserva + " no trobada en el historic de reserves: "));
             historic.setEstat(false);
             historicReservesRepository.save(historic);
+            log.info("Reserva desactivada correctament amb ID: {}", idReserva);
             return reservaRepository.save(reserva);
         } else {
+            log.warn("La reserva amb ID {} ja està anul·lada.", idReserva);
             throw new RuntimeException("La reserva ja està anul·lada");
         }
     }
 
+    /**
+     * Calcula la fiança d'una reserva tenint en compte si el client és premium.
+     *
+     * @param reserva Reserva per a la qual calcular la fiança
+     * @return Fiança calculada
+     */
     public double calculFianca(Reserva reserva) {
-
+        log.info("Calculant la fiança al servei per la reserva amb ID: {}", reserva.getIdReserva());
         Client client = reserva.getClient();
 
         boolean esClientPremium = client != null && client.isReputacio();
@@ -153,7 +212,17 @@ public class ReservaService {
         return fiancaAmbDescompte;
     }
 
+    /**
+     * Calcula el preu total final d'una reserva, tenint en compte el temps de
+     * lloguer i qualsevol penalització per retard.
+     *
+     * @param reserva Reserva per la qual calcular el preu
+     * @return Preu total de la reserva
+     * @throws IllegalArgumentException si les dates i hores són nules
+     */
     public double calculPreuTotal(Reserva reserva) {
+        log.info("Calculant el preu total al servei per la reserva amb ID: {}", reserva.getIdReserva());
+
         if (reserva.getHoraInici() == null || reserva.getHoraLliurar() == null
                 || reserva.getHoraFi() == null || reserva.getHoraRetornar() == null
                 || reserva.getDataLliurar() == null || reserva.getDataRetornar() == null) {
@@ -199,7 +268,15 @@ public class ReservaService {
         return Math.max(preuTotal, 0);
     }
 
+    /**
+     * Calcula el preu estimat d'una reserva sense tenir en compte penalitzacions
+     * per retard.
+     *
+     * @param reserva Reserva per la qual calcular el preu
+     * @return Preu de la reserva
+     */
     public double calculPreuReserva(Reserva reserva) {
+        log.info("Calculant el preu estimat per la reserva amb ID: {}", reserva.getIdReserva());
 
         log.info("Data Inici: " + reserva.getDataInici());
         log.info("Hora Inici: " + reserva.getHoraInici());
@@ -227,23 +304,56 @@ public class ReservaService {
         return Math.max(preuTotal, 0);
     }
 
+    /**
+     * Cerca les reserves associades a un client, aplicant opcionalment filtres
+     * addicionals.
+     *
+     * @param username         El nom d'usuari del client.
+     * @param searchEmail      El correu electrònic del client (opcional).
+     * @param searchId_reserva L'ID de la reserva a cercar (opcional).
+     * @param searchMatricula  La matrícula del vehicle a cercar (opcional).
+     * @return Una llista de reserves que coincideixen amb els criteris de cerca.
+     */
     public List<Reserva> cercarReservesPerClient(String username, String searchEmail, Long searchId_reserva,
             String searchMatricula) {
+        log.info("Cercant reserves per al client amb usuari: {}", username);
         return reservaRepository.findByClientUsuariAndFilters(username, searchEmail, searchId_reserva, searchMatricula);
     }
 
+    /**
+     * Cerca les reserves gestionades per un agent, aplicant opcionalment filtres
+     * addicionals.
+     *
+     * @param username         El nom d'usuari de l'agent.
+     * @param searchEmail      El correu electrònic associat a les reserves
+     *                         (opcional).
+     * @param searchId_reserva L'ID de la reserva a cercar (opcional).
+     * @param searchMatricula  La matrícula del vehicle a cercar (opcional).
+     * @return Una llista de reserves que coincideixen amb els criteris de cerca.
+     */
     public List<Reserva> cercarReservesPerAgent(String username, String searchEmail, Long searchId_reserva,
             String searchMatricula) {
+        log.info("Cercant reserves per l'agent amb usuari: {}", username);
+
         List<String> matriculesVehicles = vehicleRepository.findMatriculesByAgentUsuari(username);
         return reservaRepository.findByAgentAndFilters(username, matriculesVehicles, searchEmail, searchId_reserva,
                 searchMatricula);
     }
 
+    /**
+     * Obté una llista de dates en què un vehicle no està disponible per a noves
+     * reserves.
+     *
+     * @param matricula La matrícula del vehicle.
+     * @return Una llista de dates no disponibles en format "yyyy-MM-dd".
+     */
     public List<String> obtenerFechasNoDisponibles(String matricula) {
+        log.info("Obtenint dates no disponibles per al vehicle amb matrícula: {}", matricula);
         List<Reserva> reservas = reservaRepository.findByVehicleMatriculaEstat(matricula);
         List<String> fechasNoDisponibles = new ArrayList<>();
 
         for (Reserva reserva : reservas) {
+            log.debug("Afegint dates de reserva amb ID: {}", reserva.getIdReserva());
             LocalDate fechaInicio = reserva.getDataInici();
             LocalDate fechaFin = reserva.getDataFi();
 
